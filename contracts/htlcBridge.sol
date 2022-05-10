fff53c2398825814630a422878b8b0dc538c8f51
marcosmartinezfco <marcosmartinezfco@gmail.com>
2022-05-10 13:55:40 +0200

    Init portal, new crosschain transfer

---
+++ contracts/htlcBridge.sol
@@ -0,0 +1,52 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "../interfaces/IERC20.sol";
+
+contract htlcBridge {
+
+    event NewPortal(address indexed Sender, uint Amount, address Contract);
+
+    struct Transfer {
+        bytes32 commitment;     //merkle tree root hash
+        address sender;         //used to generate commitment (leaf)
+        address receiver;       //used to generate commitment (leaf)
+        address tokenContract;  //used to generate commitment (leaf)
+        uint amount;            //used to generate commitment (leaf)
+        bytes32 hashLock;
+        uint timeLock;
+    }
+
+    mapping(address=>Transfer) _transfers;
+    mapping(address=>bool) _hasActiveTransfer;
+    mapping(address=>mapping(address=>uint)) _lockedValue;
+
+    modifier noActiveTransfer {
+        require(_hasActiveTransfer[msg.sender] == false, "Error: Ongoing Transfer, wait until it either completes or expires");
+        _;
+    }
+
+    function initPortal(bytes32 _commitment, bytes32 _hashLock, address _tokenContract, address _receiver, uint _amount) external noActiveTransfer{
+        IERC20 tokenContract = IERC20(_tokenContract);
+        require(tokenContract.allowance(msg.sender, address(this)) >= _amount, "Error: Insuficient allowance");
+        _hasActiveTransfer[msg.sender] = true;
+        _lockedValue[msg.sender][_tokenContract] += _amount;
+        _transfers[msg.sender] = Transfer(_commitment, msg.sender, _receiver, _tokenContract, _amount, _hashLock, block.timestamp + 1 hours);
+        tokenContract.transferFrom(msg.sender, address(this), _amount);
+        emit NewPortal(msg.sender, _amount, _tokenContract);
+    }
+
+    function getTransfer(address _sender) external view
+    returns(
+        bytes32,
+        address,
+        address,
+        address,
+        uint,
+        bytes32
+    ){
+        require(_hasActiveTransfer[_sender], "Error: There aren't any ongoing transfer for the sender");
+        Transfer memory transfer = _transfers[_sender];
+        return (transfer.commitment, _sender, transfer.receiver, transfer.tokenContract, transfer.amount, transfer.hashLock);
+    }
+}
