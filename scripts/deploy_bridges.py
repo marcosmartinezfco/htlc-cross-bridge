b3895d62fd34a7e28d98803202a5b4d2724a74f1
marcosmartinezfco <marcosmartinezfco@gmail.com>
2022-05-23 12:24:22 +0200

    Deployment script

---
+++ scripts/deploy_bridges.py
@@ -0,0 +1,21 @@
+from brownie import accounts, network, config, htlcBridge, Token
+
+def main():
+    dev = accounts.add(config['wallets']['from_key'])
+
+    network.connect('ropsten-alchemy')
+    print('Network -- '+network.show_active())
+    print('Balance -- '+dev.balance())
+    ropsten = htlcBridge.deploy({'from':dev})
+    print('Contract Address -- '+ropsten.address())
+    tokenRopsten = Token.deploy("Ropsten Token Bridge", "RTB", 18, 1e18, {'from':dev})
+    tokenRopsten.transfer(dev, 18000, {'from':dev})
+    network.disconnect()
+
+    network.connect('rinkeby-alchemy')
+    print('Network -- '+network.show_active())
+    print('Balance -- '+dev.balance())
+    rinkeby = htlcBridge.deploy({'from':dev})
+    print('Contract Address -- '+rinkeby.address())
+    tokenRinkeby = Token.deploy("Rinkeby Token Bridge", "RTB", 18, 1e18, {'from':dev})
+    tokenRinkeby.transfer(dev, 18000, {'from':dev})
